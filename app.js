a6de4c75ac5887c884ce4338eaf75e01cad10382
Ulisses Malanski <malanskiwork@gamil.com>
2023-07-03 20:41:18 -0300

    create StarWars Api app

---
+++ app.js
@@ -0,0 +1,117 @@
+
+// Dark Mode switcher
+function changeMode() {
+  changeClasses()
+  changeText()
+  console.log('cliquei')
+}
+
+function changeClasses() {
+  modeBtn.classList.toggle(darkModeClass); 
+  Btn.classList.toggle(darkModeClass); 
+  container.classList.toggle(darkModeClass); 
+  modeTitle.classList.toggle(darkModeClass); 
+  body.classList.toggle(darkModeClass); 
+  footer.classList.toggle(darkModeClass); 
+  
+}
+
+function changeText() {
+  const lightText = 'Light Mode'
+  const darkText  = 'Dark Mode'
+  const lightIcon = `<i style='color:yellow' class="fa-solid fa-sun"></i>`
+  const darkIcon  = `<i class="fa-solid fa-moon"></i>`
+  if (body.classList.contains(darkModeClass)) {
+      modeTitle.innerHTML = lightText
+      modeBtn.innerHTML = lightIcon
+      return
+  } 
+  modeTitle.innerHTML = darkText
+  modeBtn.innerHTML = darkIcon
+}
+
+const darkModeClass = 'dark-mode'
+
+const modeBtn    = document.getElementById('modeToggle')
+
+const modeTitle  = document.getElementById('mode-title')
+const body       = document.getElementsByTagName('body')[0]
+const footer     = document.getElementsByTagName('footer')[0]
+
+const container        = document.getElementById('info-container')
+const Btn        = document.getElementById('button')
+
+
+modeBtn.addEventListener('click', changeMode)
+
+
+let button       = document.querySelector('#button')
+let name         = document.querySelector('#name' )
+let height       = document.querySelector('#height')
+let mass         = document.querySelector('#mass')
+let birth_year   = document.querySelector('#birth_year')
+let gender       = document.querySelector('#gender')
+let hair_color   = document.querySelector('#hair_color')
+let skin_color   = document.querySelector('#skin_color')
+let eye_color    = document.querySelector('#eye_color')
+let homeworld    = document.querySelector('#homeworld')
+
+
+
+async function getInfo() {
+
+  updateInfoLoading()
+  await fakePromise();
+
+  let randomNumber = Math.floor((Math.random() * 88) + 1)
+
+  let apiUrl = 'https://swapi.dev/api/people/' + randomNumber
+
+
+  axios.get(apiUrl).then(function(response){
+      updateInfo(response.data)
+  }).catch(e => {
+      console.log(updateInfoError())
+  })
+}
+
+function updateInfo(data) {
+  name.innerText       = 'Name: '   + data.name
+  height.innerText     = 'Height: ' + data.height + ' cm'
+  mass.innerText       =       `Mass: ${data.mass}`
+  birth_year.innerText = `Birth year: ${data.birth_year}`
+  gender.innerText     =     `Gender: ${data.gender}`
+  hair_color.innerText = 'Hair color: ' + data.hair_color
+  skin_color.innerText = 'Skin solor: ' + data.skin_color
+  eye_color.innerText  = 'Eye color: '  + data.eye_color
+  homeworld.innerText  = 'Homeworld: '  + data.homeworld
+}   
+
+
+function updateInfoError() {
+  name.innerText       = 'Oh no there is no data about this person.' 
+  height.innerText     = ''
+  mass.innerHTML       = '<img src="/no-data.jpeg" style="width:100%;">'
+  birth_year.innerText = ''
+  gender.innerText     = 'Try again!'
+  hair_color.innerText = ''
+  skin_color.innerText = ''
+  eye_color.innerText  = ''
+  homeworld.innerText  = ''
+}   
+
+function updateInfoLoading() {
+  name.innerHTML       = 'We are searching for, it please wait...' 
+  height.innerHTML     = '<img src="/leia.gif" style="width: 100%; heigth: 80%;">'
+  mass.innerText       = ''
+  birth_year.innerHTML = ''
+  gender.innerText     = ''
+  hair_color.innerHTML = ''
+  skin_color.innerText = ''
+  eye_color.innerHTML  = ''
+  homeworld.innerText  = ''
+}
+
+const fakePromise = () => new Promise((resolve) => setTimeout(resolve, 3000));
+
+button.addEventListener('click', getInfo);
---
+++ script.js
@@ -0,0 +1,93 @@
+// const BASE_URL = 'https://swapi.dev/api/species/${id}/';
+// const catBtn = document.getElementById('change-cat');
+
+// const getCats = async () => {
+//     try {
+//         const data = await fetch(BASE_URL);
+//         const response = await data.response();
+
+//         return response.species.id;
+
+//     } catch (error) {
+//         console.log(error.massage)
+//     }
+// };
+
+// const loadImg = async () => {
+//     const catImg = document.getElementById('image');
+//     catImg.src = await getCats();
+// }
+
+// catBtn.addEventListener('click', loadImg);
+
+// loadImg();
+
+
+// function ajax_get(url, callback) {
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function() {
+//       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//         console.log('responseText:' + xmlhttp.responseText);
+//         try {
+//           var data = JSON.parse(xmlhttp.responseText);
+//         } catch (err) {
+//           console.log(err.message + " in " + xmlhttp.responseText);
+//           return;
+//         }
+//         callback(data);
+//       }
+//     };
+  
+//     xmlhttp.open("GET", url, true);
+//     xmlhttp.send();
+//   }
+  
+//   ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function(data) {
+//     document.getElementById("id").innerHTML = data[0]["id"];
+//     document.getElementById("url").innerHTML = data[0]["url"];
+  
+//     var html = '<img src="' + data[0]["url"] + '">';
+//     document.getElementById("image").innerHTML = html;
+//   });
+  
+
+
+
+class Pokemon {
+  constructor(nome, url){
+      this.nome = nome;
+      this.url = url;
+      this.id = this.url.replace('https://swapi.dev/api/species/','').replace('/', '');
+      this.imagem = `
+          https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${this.id}.png`;
+      this.imagem3 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${this.id}.gif`;   
+      
+      this.preco = Math.floor(Math.random() * 100).toFixed(2);
+      this.precoDesc = (this.preco * 0.8).toFixed(2);
+      this.precoParc = (this.preco / 12).toFixed(2);
+  }
+
+  html() {
+      const pokeDiv = document.createElement('div');
+      pokeDiv.className = 'poke';
+      pokeDiv.innerHTML = `
+          <a href="pokemon.html?id=${this.id}">
+              <div class="card-feet">
+                  <h2>${this.nome}</h2>
+                  <img class="poke-pic" src="${this.imagem}" alt="${this.nome}">
+                  <p class="money old-price"><small>R$${this.preco}</small></p>
+                  <p class="money actual-price"><small>R$</small>${this.precoDesc}</p>
+                  <p class="money parcela-price"><small>12x R$ </small>${this.precoParc}</p><br>
+                  <div>
+                      <button data-id="${this.id}" class="btn-buy">
+                      <img data-id="${this.id}" src="${this.imagem3}" title="Buy ${this.nome} Now">
+                      <!--<i class="far fa-dot-circle"></i>-->
+                      <span data-id="${this.id}" class="buy">Buy</span>
+                      </button>
+                  </div>
+              </div>
+          </a>
+      `;
+      return pokeDiv;
+  }
+}
